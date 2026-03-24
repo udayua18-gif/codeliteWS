eecb8b007a958d6a162778b6fc733510f2046f9e
udayua18-gif <udayua18@gmail.com>
2026-03-23 20:06:05 -0400

    adding projet3

--- project3/main.cpp
+++ project3/main.cpp
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+#include<string>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+using namespace std;
+
+int main() {
+    string name {"uday"};
+    int num1 {10};
+    int num2 {20};
+    
+   
+    cout << "name is " << name << endl;
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    
+    
+    cout<<endl;
+    return 0;
+    
 }
